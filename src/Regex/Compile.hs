c86248fa35a2dc5c938056a74d680f8b3f31c8e8
Chris Penner <christopher.penner@gmail.com>
2017-04-25 18:18:03 -0600

    Working list monad impl

--- src/Regex/Compile.hs
+++ src/Regex/Compile.hs
@@ -1,14 +1,45 @@
 module Regex.Compile
-  ( RegexPattern(..)
+  ( match
   ) where
 
--- import Regex.Parse
+import Regex.Parse
+-- import Data.Foldable
+import Control.Monad.State
 
-data RegexPattern
-  = ManyOf RegexPattern
-  | Group RegexPattern
-  | Singleton RegexPattern
-  | Optional RegexPattern
+-- import Text.Parsec.Error
 
--- compile :: [RegexToken] -> RegexPattern
--- compile = undefined
+data RState = RState
+  { current :: String
+  , groups :: Groups
+  , leftover :: String
+  }
+
+type Leftover = String
+type Current = String
+type Match = (Groups, Current, String)
+-- type Pattern = String
+type Groups = [String]
+
+-- match :: Pattern -> String -> Either ParseError Match
+-- match pattern str = flip match' str <$> parseRegex pattern
+
+
+
+match :: Expr -> Groups -> String -> [Match]
+match (Atom c) groups (x:xs)
+  | c == x = [(groups, [x], xs)]
+  | otherwise = []
+match Wildcard groups (x:xs) = [(groups, [x], xs)]
+match (OneOf terms) groups str = do
+  term <- terms
+  match term groups str
+
+match (Group next) groups str = fmap addGroup $ match next groups str
+  where addGroup (grps, cur, leftover) = (grps ++ [cur], cur, leftover)
+
+match (Terms terms) groups str = foldl go [(groups, "", str)] terms
+  where go matches term = do
+            (grps, cur, leftover) <- matches
+            (grps', cur', leftover') <- match term grps leftover
+            return (grps', cur++cur', leftover')
+match _ _ [] = []
--- src/Regex/Parse.hs
+++ src/Regex/Parse.hs
@@ -1,17 +1,17 @@
 module Regex.Parse
   ( parseRegex
-  , Structure(..)
+  , Expr(..)
   ) where
 
 import Text.Read (readMaybe)
 import Text.Parsec
 import Text.Parsec.String
 
-data Structure
-  = Group Structure
-  | Terms [Structure]
-  | OneOf [Structure]
-  | Repetition Structure Int (Maybe Int)
+data Expr
+  = Group Expr
+  | Terms [Expr]
+  | OneOf [Expr]
+  | Repetition Expr Int (Maybe Int)
   | BackRef Int
   | Range String
   | Wildcard
@@ -23,24 +23,24 @@ data Structure
 specialChars :: String
 specialChars = "\\[]|{}().+*?"
 
-parseRegex :: String -> Either ParseError Structure
+parseRegex :: String -> Either ParseError Expr
 parseRegex = runParser regex () "Regex"
 
-regex :: Parser Structure
-regex = structure <* eof
+regex :: Parser Expr
+regex = expr <* eof
 
-structure :: Parser Structure
-structure = OneOf <$> terms `sepBy1` char '|'
+expr :: Parser Expr
+expr = OneOf <$> terms `sepBy1` char '|'
 
-terms :: Parser Structure
+terms :: Parser Expr
 terms = Terms <$> many1 term
 
-term :: Parser Structure
+term :: Parser Expr
 term = do
   re <- choice [group, range, token']
   option re (quantifier re <|> repetition re)
 
-quantifier :: Structure -> Parser Structure
+quantifier :: Expr -> Parser Expr
 quantifier re = do
   c <- oneOf "?*+"
   return $ case c of
@@ -49,7 +49,7 @@ quantifier re = do
              '+' -> Repetition re 1 Nothing
              _ -> error "Impossible pattern match fail"
 
-repetition :: Structure -> Parser Structure
+repetition :: Expr -> Parser Expr
 repetition re =
   between (char '{') (char '}') $ do
     start <- read <$> many1 digit
@@ -59,10 +59,10 @@ repetition re =
       readMaybe <$> many digit
     return $ Repetition re start end
 
-group :: Parser Structure
-group = Group <$> between (char '(') (char ')') structure
+group :: Parser Expr
+group = Group <$> between (char '(') (char ')') expr
 
-token' :: Parser Structure
+token' :: Parser Expr
 token' = start <|> end <|> wildcard <|> atom <|> try escaped <|> backRef
   where
     start = char '^' *> return Start
@@ -72,7 +72,7 @@ token' = start <|> end <|> wildcard <|> atom <|> try escaped <|> backRef
     escaped = Atom <$> (char '\\' *> oneOf specialChars)
     backRef = BackRef . read <$> (char '\\' *> many1 digit)
 
-range :: Parser Structure
+range :: Parser Expr
 range = Range . concat <$> between (char '[') (char ']') (many1 (try span' <|> lit))
   where
     lit = (:[]) <$> noneOf "]"
